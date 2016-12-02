0e714ffc2ee6b69942cf586dd8ef3ed4c151f509
Kiritoinder <inderbharaj@ymail.com>
2016-12-02 17:33:23 -0500

    Add project files.

---
+++ Comp229-FinalTeamProject/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comp229_FinalTeamProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Title = "About";
+            ViewBag.Inder = "Inder";
+
+            return View();
+        }
+
+        [Authorize]
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [Authorize(Users = "IIKA")]
+        public ActionResult IIKASecrets()
+        {
+            ViewBag.Message = "Your Administration page.";
+            return View();
+        }
+
+
+        [Authorize(Roles = "Administrators")]
+        public ActionResult AdministratorSecrets()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Comp229-FinalTeamProject/Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comp229_FinalTeamProject.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is my Store Index Page";
+            return View();
+        }
+
+        public ActionResult Browse(String genre)
+        {
+            ViewBag.Message = HttpUtility.HtmlEncode("Store.Browse, Genre= " + genre);
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            ViewBag.Message = HttpUtility.HtmlEncode("Store.Detsils, id= " + id);
+            return View();
+        }
+    }
+}
